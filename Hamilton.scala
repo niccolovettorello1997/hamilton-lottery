1f1d93cc6e3d6864bc325ce428e51df67261dadc
niccolovettorello1997 <n.vettorello@cartaidentitalimentare.com>
2023-04-11 19:27:57 +0200

    add initial main body

---
+++ Hamilton.scala
@@ -0,0 +1,10 @@
+object Hamilton {
+    // Take in input the number of entrants
+    // Compute the probability of winning
+    // Compute the estimated number of times that one has to play in order to have 99% of winning chance
+    // Run the lottery simulation
+    // Show the number of attempts tried before winning
+    def main(args: Array[String]) = {
+        var entrants: String = scala.io.StdIn.readLine();
+    }
+}
